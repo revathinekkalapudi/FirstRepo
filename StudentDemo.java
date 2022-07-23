bcf8b3408c5b933474083f6904904804709a39bb
revathinekkalapudi <revathi1245@sasi.ac.ingit config --global user.email revathi1245@sasi.ac.ingit config --global user.email revathi1245@sasi.ac.in>
2022-07-23 14:44:17 +0530

    revathi

---
+++ StudentDemo.java
@@ -0,0 +1,22 @@
+class StudentDemo
+{
+void Name()
+{
+	System.out.println("Madhuri");
+}
+void  RollNo()
+{
+	System.out.println("20k61A1246");
+}
+void Gender()
+{
+	System.out.println("Female");
+}
+	public static void main(String args[])
+	{
+		StudentDemo sd = new StudentDemo;
+		sd Name();
+		sd RollNo();
+		sd Gender();
+	}
+}
